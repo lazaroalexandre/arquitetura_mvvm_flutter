7245b52220eae0420ea4d0da7a6ad35776218952
lazaroalexandre <lazaroalexandre2022@gmail.com>
2024-04-27 21:27:46 -0300

    Remoção do provider

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,40 +1,22 @@
 import 'package:arquitetura_flutter/app/presentation/home/controllers/home_controller.dart';
-import 'package:arquitetura_flutter/app/presentation/home/controllers/user_controller.dart';
 import 'package:arquitetura_flutter/app/presentation/home/pages/home_page.dart';
-import 'package:arquitetura_flutter/app/repositories/user_repository_impl.dart';
-import 'package:arquitetura_flutter/app/services/client/client_service_impl.dart';
-import 'package:arquitetura_flutter/app/viewmodels/user_viewmodel.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<UserController>.value(
-          value: UserController(
-            viewmodel: UserViewmodel(
-              userRepository: UserRepositoryImpl(
-                clientInterface: ClientServiceImpl(),
-              ),
-            ),
-          ),
-        ),
-      ],
-      child: ValueListenableBuilder<bool>(
-        valueListenable: HomeController.instance.themeSwitch,
-        builder: (_, isDark, __) {
-          return MaterialApp(
-            title: 'Flutter Demo',
-            debugShowCheckedModeBanner: false,
-            theme: isDark ? ThemeData.dark() : ThemeData.light(),
-            home: const HomePage(),
-          );
-        },
-      ),
+    return ValueListenableBuilder<bool>(
+      valueListenable: HomeController.instance.themeSwitch,
+      builder: (_, isDark, __) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: isDark ? ThemeData.dark() : ThemeData.light(),
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
--- lib/app/presentation/home/components/list_user_widget.dart
+++ lib/app/presentation/home/components/list_user_widget.dart
@@ -2,43 +2,40 @@ import 'package:arquitetura_flutter/app/models/user_model.dart';
 import 'package:arquitetura_flutter/app/presentation/home/components/list_tile_widget.dart';
 import 'package:arquitetura_flutter/app/presentation/home/controllers/user_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ListUserWidget extends StatelessWidget {
-  const ListUserWidget({super.key});
+  ListUserWidget({super.key});
+
+  final UserController controller = UserController.instance;
 
   @override
   Widget build(BuildContext context) {
-    final userController = Provider.of<UserController>(context, listen: false);
-    userController.getUsers();
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Consumer<UserController>(
-        builder: (_, value, __) {
-          if (value.isLoading.value) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (value.error.value.isNotEmpty) {
-            return Center(
-              child: Text(value.error.value),
-            );
-          }
-          if (value.users.value.isEmpty) {
-            return const Center(
-              child: Text("Não existem contatos registrados!"),
-            );
-          }
-          return ListView.builder(
-            itemCount: value.users.value.length,
-            itemBuilder: (_, index) {
-              final UserModel model = value.users.value[index];
-              return ListTileWidget(model: model);
-            },
-          );
-        },
-      ),
+    return ValueListenableBuilder(
+      valueListenable: controller.users,
+      builder: (_, value, __) {
+             if (controller.isLoading.value) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (controller.error.value.isNotEmpty) {
+                return Center(
+                  child: Text(controller.error.value),
+                );
+              }
+              if (controller.users.value.isEmpty) {
+                return const Center(
+                  child: Text("Não existem contatos registrados!"),
+                );
+              }
+        return ListView.builder(
+          itemCount: value.length,
+          itemBuilder: (_, index) {
+            final UserModel model = value[index];
+            return ListTileWidget(model: model);
+          },
+        );
+      },
     );
   }
 }
