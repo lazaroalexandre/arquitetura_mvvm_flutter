f5d8b8fb3c91a44cdf2466fb8a630cf2892631dc
lazaroalexandre <lazaroalexandre2022@gmail.com>
2024-05-15 17:52:09 -0300

    Mudança de modulos para incluir as rotas da api

--- lib/app/modules/home/repositories/user_repository_impl.dart
+++ lib/app/modules/home/repositories/user_repository_impl.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:arquitetura_flutter/app/core/exceptions/exception_not_found.dart';
-import 'package:arquitetura_flutter/app/modules/home/api/routes_api.dart';
+import 'package:arquitetura_flutter/app/core/api/routes_api.dart';
 import 'package:arquitetura_flutter/app/modules/home/models/user_model.dart';
 import 'package:arquitetura_flutter/app/modules/home/services/client/client_service.dart';
 import 'package:dio/dio.dart';
