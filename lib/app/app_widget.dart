d09e0efed2085ee2fa54dd4549fde00cc5b32759
lazaroalexandre <lazaroalexandre2022@gmail.com>
2025-03-28 18:01:48 -0300

    feat: adicionado o localizationsDelegates e o supportedLocales

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,13 +1,23 @@
+import 'package:arquitetura_flutter/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: L10n.all,
     );
   }
 }
